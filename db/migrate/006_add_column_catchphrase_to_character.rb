2abe3206f48f19f861e99c42dff83a2b2e050307
Omar Nava <omarnavaalvarez@gmail.com>
2018-04-02 20:12:28 +0000

    automatically backed up by learn

--- db/migrate/006_add_column_catchphrase_to_character.rb
+++ db/migrate/006_add_column_catchphrase_to_character.rb
@@ -1,5 +1,5 @@
 class AddColumnCatchphraseToCharacter < ActiveRecord::Migration[4.2]
   def change
-    add column :characters, :catchphrase, :string
+    add_column :characters, :catchphrase, :string
   end
 end
