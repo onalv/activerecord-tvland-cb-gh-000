f4d32142b0c6cf76f4af9aad378ef1e3d8249c0b
Omar Nava <omarnavaalvarez@gmail.com>
2018-04-02 20:28:14 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,8 @@
 class Character < ActiveRecord::Base
-  belongs_to :actor
   belongs_to :show
+  belongs_to :actor
 
-  def say_that_thing_you_say(phrase)
-    puts phrase
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
   end
 end
