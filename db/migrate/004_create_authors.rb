fe5ceb44f9642f787d73dd2bc5d8d60142276721
Omar Nava <omarnavaalvarez@gmail.com>
2018-04-02 20:14:57 +0000

    automatically backed up by learn

--- db/migrate/004_create_authors.rb
+++ db/migrate/004_create_authors.rb
@@ -1,6 +1,6 @@
 class CreateAuthors < ActiveRecord::Migration[4.2]
   def change
-    create_table :author do |t|
+    create_table :authors do |t|
       t.string :first_name
       t.string :last_name
     end
