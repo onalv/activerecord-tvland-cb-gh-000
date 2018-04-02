769c3fd76a4aae366369fbaf1f8e711b9529ddff
Omar Nava <omarnavaalvarez@gmail.com>
2018-04-02 19:56:03 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,7 +2,7 @@ class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :last_name
+      t.integer :show_id
     end
   end
 end
